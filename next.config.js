caa0b2170b58c6a5eb08c9faaf295114c0578b7c
kanandev2024 <kananramazandev@gmail.com>
2024-03-19 22:46:01 -0700

    update:next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+
 const path = require('path')
  
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
-  }
+  },
+  output:'export',
 }
