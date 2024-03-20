8f236337c29eae56d2a5d9c03033ee40dae7ef65
kanandev2024 <kananramazandev@gmail.com>
2024-03-20 00:56:10 -0700

    feat: ready to deploy

--- app/components/homepage/about/index.jsx
+++ app/components/homepage/about/index.jsx
@@ -23,13 +23,13 @@ function AboutSection() {
           </p>
         </div>
         <div className="flex justify-center order-1 lg:order-2">
-          <Image
+          {/* <Image
             src={personalData.profile}
             width={280}
             height={280}
             alt="Kanan Ramazanov"
             className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
-          />
+          /> */}
         </div>
       </div>
     </div>
--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
-/**
- * @type {import('next').NextConfig}
- */
-
 const path = require('path')
  
 module.exports = {
+  output:"export",
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
-  output:'export',
 }
